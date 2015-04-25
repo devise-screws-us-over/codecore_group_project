5b30dc3aa4751d13af841f60efc98673354a58c8
Kosuke <kondo1017@gmail.com>
2015-04-24 19:55:17 -0700

    Add something on Controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,17 +4,32 @@ class TeamsController < ApplicationController
   end
 
   def create
+    @team = Team.find(params[:team_id])
+
+    if @teeam.save
+      redirect_to @team
+    else
+      render 'new'
+    end
   end
 
   def show
+    @team = Team.find(params[:team_id])
   end
 
   def update
-  end
-
-  def edit
+    @post = Team.find(params[:id])
+    if @team.update(params[:team].permit(:name))
+      redirect_to @team
+    else
+      render 'edit'
+    end
   end
 
   def destroy
   end
+
+  private
+  def team_params
+    prams.require(:team).permit(:name)
 end
