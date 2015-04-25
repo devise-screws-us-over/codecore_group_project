c072888a0daf276250d4918db87755f20e4ad65c
Jared Kalef <jkalef@gmail.com>
2015-04-25 12:53:09 -0700

    fixes and user routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if profile.avatar_url.present?
       profile.avatar_url
     else
-      default_url = "app/assets/images/guest.png"
+      default_url = asset_url("guest.png")
       gravatar_id  = Digest::MD5.hexdigest(profile.user.email.downcase)
       "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
     end
