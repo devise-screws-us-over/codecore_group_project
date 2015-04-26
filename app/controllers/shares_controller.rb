87cfe56699905d86b67bcf923e3156cc413d72a1
Hon Lam <lamh85@gmail.com>
2015-04-26 16:46:54 -0700

    Resolving merge conflicts

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,23 +1,23 @@
 class SharesController < ApplicationController
 
- def create
-   @idea           = Idea.find params[:idea_id]
-   teams           = params["share"]["team_ids"]
+  def create
+    @idea           = Idea.find params[:idea_id]
+    teams           = params["share"]["team_ids"]
 
-   if @idea && teams && teams.count > 1 
-      teams.each do |team|
-        if !team.blank?
-          Share.create(team_id: team, idea_id: @idea.id)
+    if @idea && teams && teams.count > 1 
+        teams.each do |team|
+          if !team.blank?
+            Share.create(team_id: team, idea_id: @idea.id)
+          end
         end
-      end
-  else
-    error = "yourmissing something"
-  end
+    else
+      error = "yourmissing something"
+    end
   #  @share.team     = team
   #  @share          = @idea.shares.new
   #  @share.idea     = @idea
 
-   respond_to do |format|
+    respond_to do |format|
       # if error
         format.html {redirect_to @idea, notice: "Successfully Shared"}
       # end
