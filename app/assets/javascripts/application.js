04018f2a11519dfb5fe2461901baf3241b94ae16
Diederik Kroondijk <dkroondijk@hotmail.com>
2015-04-26 15:53:06 -0700

    fix jquery turbolinks conflict, added gem jquery-turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,12 @@
 // about supported directives.
 //
 //= require jquery
-//= require best_in_place
+//= require jquery.turbolinks
 //= require jquery_ujs
-
+//= require best_in_place
 //= require jquery-ui/sortable
-//= require turbolinks
 //= require bootstrap-sprockets
+//= require turbolinks
 //= require_tree .
 
 $(document).ready(function(){
