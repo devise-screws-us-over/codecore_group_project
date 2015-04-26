7f3182db45acbc6348100e2d163bb7335a73d2fa
Hon Lam <lamh85@gmail.com>
2015-04-25 17:42:41 -0700

    Now can fully create rows in Invitation table

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,13 +1,22 @@
 class InvitationsController < ApplicationController
 
+  before_action :authenticate_user!
+
   def create
     @team = Team.find params[:team_id]
     @new_array = (params[:invitation][:array]).split(",")
+    # byebug
+    # john@john.com      ,       jane@jane.com
     # render text: @new_array
     @new_array.each do |email|
       @invitation = Invitation.new
+      # Assign attributes
       @invitation.team = @team
-      @invitation.recipient = email
+      @invitation.recipient = email.delete(" ")
+      @invitation.user = current_user
+      @invitation.key = SecureRandom.hex
+      @invitation.is_sent = false
+      # Save
       @invitation.save
     end
     redirect_to edit_team_path(@team)
